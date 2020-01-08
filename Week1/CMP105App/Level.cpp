92858f1d13b6a07d1c8cecd4cca7c9a69655c800
Paul Robertson <p.robertson@abertay.ac.uk>
2020-01-08 15:06:24 +0000

    Initial commit
    Added application.

---
+++ Week1/CMP105App/Level.cpp
@@ -0,0 +1,46 @@
+#include "Level.h"
+
+Level::Level(sf::RenderWindow* hwnd)
+{
+	window = hwnd;
+	
+	// initialise game objects
+	rect.setSize(sf::Vector2f(50, 5));
+	rect.setPosition(100, 100);
+	rect.setFillColor(sf::Color::Red);
+}
+
+Level::~Level()
+{
+}
+
+// handle user input
+void Level::handleInput()
+{
+
+}
+
+// Update game objects
+void Level::update()
+{
+	
+}
+
+// Render level
+void Level::render()
+{
+	beginDraw();
+
+	endDraw();
+}
+
+void Level::beginDraw()
+{
+	window->clear(sf::Color(100, 149, 237));
+}
+
+// Ends rendering to the back buffer, and swaps buffer to the screen.
+void Level::endDraw()
+{
+	window->display();
+}
---
+++ Week1/CMP105App/Level.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string.h>
+#include <iostream>
+
+
+class Level{
+public:
+	Level(sf::RenderWindow* hwnd);
+	~Level();
+
+	void handleInput();
+	void update();
+	void render();
+
+private:
+	void beginDraw();
+	void endDraw();
+	sf::RenderWindow* window;
+
+	sf::RectangleShape rect;
+	sf::CircleShape circle;
+	sf::Font font;
+};
